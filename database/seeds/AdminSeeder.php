0ede6cd4694f47d0f87041f20ea45255aff2083b
Azibbe <syarumie@gmail.com>
2019-07-10 16:52:34 +0800

    Seed with new user and role

--- database/seeds/AdminSeeder.php
+++ database/seeds/AdminSeeder.php
@@ -15,37 +15,64 @@ class AdminSeeder extends DatabaseSeeder {
 		
 
         // create 2 users
+		$superadmin = Sentinel::registerAndActivate(array(
+			'email'       => 'superadmin@superadmin.com',
+			'password'    => "superadmin",
+			'first_name'  => 'Saya',
+			'last_name'   => 'SuperAdmin',
+		));
 		$admin = Sentinel::registerAndActivate(array(
 			'email'       => 'admin@admin.com',
 			'password'    => "admin",
-			'first_name'  => 'John',
-			'last_name'   => 'Doe',
+			'first_name'  => 'Saya',
+			'last_name'   => 'Admin',
 		));
-        $user = Sentinel::registerAndActivate(array(
-            'email'       => 'user@user.com',
-            'password'    => "user",
-            'first_name'  => 'John',
-            'last_name'   => 'Doe',
+		$penyelia = Sentinel::registerAndActivate(array(
+			'email'       => 'penyelia@penyelia.com',
+			'password'    => "penyelia",
+			'first_name'  => 'Saya',
+			'last_name'   => 'Penyelia',
+		));
+        $kerani = Sentinel::registerAndActivate(array(
+            'email'       => 'kerani@kerani.com',
+            'password'    => "kerani",
+            'first_name'  => 'Saya',
+            'last_name'   => 'Kerani',
         ));
 
         // create 2 roles
+        $superadminRole = Sentinel::getRoleRepository()->createModel()->create([
+			'name' => 'SuperAdmin',
+			'slug' => 'superadmin',
+			'permissions' => array('superadmin' => 1)
+		]);
         $adminRole = Sentinel::getRoleRepository()->createModel()->create([
 			'name' => 'Admin',
 			'slug' => 'admin',
 			'permissions' => array('admin' => 1)
 		]);
+        $penyeliaRole = Sentinel::getRoleRepository()->createModel()->create([
+			'name' => 'Penyelia',
+			'slug' => 'penyelia',
+			'permissions' => array('penyelia' => 1)
+		]);
 
-        $userRole = Sentinel::getRoleRepository()->createModel()->create([
-			'name'  => 'User',
-			'slug'  => 'user',
-            'permissions' => array('user' => true)
+        $keraniRole = Sentinel::getRoleRepository()->createModel()->create([
+			'name'  => 'Kerani',
+			'slug'  => 'kerani',
+            'permissions' => array('kerani' => true)
 		]);
 
         // add user to user role and admin to admin role
-        $user->roles()->attach($userRole);
+        $kerani->roles()->attach($keraniRole);
 		$admin->roles()->attach($adminRole);
+		$superadmin->roles()->attach($superadminRole);
+		$penyelia->roles()->attach($penyeliaRole);
 
 		$this->command->info('Admin User created with username admin@admin.com and password admin');
+        $this->command->info('SuperAdmin User created with username superadmin@superadmin.com and password superadmin');
+        $this->command->info('Penyelia User created with username penyelia@penyelia.com and password penyelia');
+        $this->command->info('Kerani User created with username kerani@kerani.com and password kerani');
 	}
 
 }
